3aa68a6b0adbdc4750592cbf539006d71964eba5
Noba <jolechallinor3@gmail.com>
2026-02-03 17:51:08 +0000

    Added service locator

---
+++ Core/Services/ServiceMonoBehaviour.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+/// <summary>
+/// Used by the Service Locator but not necessary as it is a templated class.
+/// Base class for services like the object pool manager.
+/// </summary>
+/// 
+public class ServiceMonoBehaviour : MonoBehaviour
+{
+    protected virtual void Awake()
+    {
+    }
+
+    protected virtual void Start()
+    {
+
+    }
+
+    protected virtual void Update()
+    {
+    }
+}
